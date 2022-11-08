fcbaacf4b22007a6d8efe151a5e85d2554a8e16d
Caliph Dev <admin@caliph.my.id>
2022-11-08 15:57:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-module.exports.admin_id = [{ id: 123456789, username: "username" }];
-module.exports.bot_token = "Your Bot Token";
+module.exports.admin_id = [{ id: 1925756864, username: "caliphdev" }];
+module.exports.bot_token = "5600609530:AAGAjMAf1JOI0j2yq3Ex9-Qqk5tyQh2EHVc";
