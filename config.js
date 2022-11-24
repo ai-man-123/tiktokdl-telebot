3baa3b2a43dcc609c2eaaba89feeb9dccfd3495b
AIMAN *͘⁴̅⁰͍⁴̵〆* <Lynxaiman@yahoo.com>
2022-11-24 20:28:17 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-module.exports.admin_id = [{ id: 123456789, username: "Boruto_donic_bot" }];
+module.exports.admin_id = [{ id: 123456789, username: "aiman1_23" }];
 module.exports.bot_token = "1715020456:AAH3uzgnnLLWApNf3od9QHp2eSFfHvAX81U";
